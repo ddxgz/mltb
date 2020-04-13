8b3acd63a1a328f673ff90813451373aa9356c73
pcx <ddxgz19880813@gmail.com>
2020-04-13 08:59:28 +0200

    Add timer

--- mltb/utils.py
+++ mltb/utils.py
@@ -1,6 +1,7 @@
 import os
 from os.path import join
 import shutil
+from time import time
 import logging
 import json
 
@@ -93,3 +94,32 @@ def setup_logging(log_path=None, log_level='DEBUG', logger=None, fmt=None):
         file_handler.setFormatter(fmt)
         logger.addHandler(file_handler)
         logger.info('Log file is %s', log_path)
+
+
+class Timer:
+    """Record multiple running times."""
+
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        # Start the timer
+        self.tik = time.time()
+
+    def stop(self):
+        # Stop the timer and record the time in a list
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        # Return the average time
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        # Return the sum of time
+        return sum(self.times)
+
+    def cumsum(self):
+        # Return the accumulated times
+        return np.array(self.times).cumsum().tolist()
